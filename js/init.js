1e71ac1cca2c418dfc0b0f56b4c848ac86bfad41
terrator <jterrator@gmail.com>
2015-03-04 10:32:25 -0500

    Menu
    Added floating menu

--- js/init.js
+++ js/init.js
@@ -20,12 +20,61 @@
 		reset: 'full',
 		containers: '100%',
 		breakpoints: {
-			global: { href: 'css/style.css', grid: { gutters: ['2.5em', 0] } },
-			xlarge: { media: '(max-width: 1800px)', href: 'css/style-xlarge.css' },
-			large: { media: '(max-width: 1280px)', href: 'css/style-large.css', grid: { gutters: ['2em', 0] } },
-			medium: { media: '(max-width: 980px)', href: 'css/style-medium.css'},
-			small: { media: '(max-width: 736px)', href: 'css/style-small.css', grid: { gutters: ['1.5em', 0], zoom: 2 }, viewport: { scalable: false } },
-			xsmall: { media: '(max-width: 480px)', href: 'css/style-xsmall.css', grid: { zoom: 3 } }
+			global: { 
+                href: 'css/style.css', 
+                grid: { gutters: ['2.5em', 0] } 
+            },
+			xlarge: { 
+                media: '(max-width: 1800px)', 
+                href: 'css/style-xlarge.css' 
+            },
+			large: { 
+                media: '(max-width: 1280px)', 
+                href: 'css/style-large.css', 
+                grid: { gutters: ['2em', 0] } 
+            },
+			medium: { 
+                media: '(max-width: 980px)', 
+                href: 'css/style-medium.css'
+            },
+			small: { 
+                media: '(max-width: 736px)', 
+                href: 'css/style-small.css', 
+                grid: { gutters: ['1.5em', 0], zoom: 2 }, 
+                viewport: { scalable: false } 
+            },
+			xsmall: { 
+                media: '(max-width: 480px)', 
+                href: 'css/style-xsmall.css', 
+                grid: { zoom: 3 } 
+            }
+		},
+		plugins: {
+			layers: {
+//				config: {
+//					mode: 'transform'
+//				},
+				navPanel: {
+					animation: 'pushX',
+					breakpoints: 'medium',
+					clickToHide: true,
+					height: '100%',
+					hidden: true,
+					html: '<div data-action="moveElement" data-args="nav"></div>',
+					orientation: 'vertical',
+					position: 'top-left',
+					side: 'left',
+					width: 200
+				},
+				navButton: {
+					breakpoints: 'medium',
+					height: '4em',
+					html: '<span class="toggle" data-action="toggleLayer" data-args="navPanel"></span>',
+					position: 'top-left',
+					side: 'top',
+					width: '6em'
+				}
+			}
 		}
 	});
 
